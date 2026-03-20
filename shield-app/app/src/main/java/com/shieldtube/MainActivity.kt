24cbd9194c1be9c042bc8e5a2d0a6907d7d660e9
Mohammad Asjad <asjad.august@gmail.com>
2026-03-20 04:16:30 +0100

    feat: wire MainActivity to BrowseFragment for browse experience

--- shield-app/app/src/main/java/com/shieldtube/MainActivity.kt
+++ shield-app/app/src/main/java/com/shieldtube/MainActivity.kt
@@ -2,14 +2,14 @@ package com.shieldtube
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import com.shieldtube.player.PlaybackFragment
+import com.shieldtube.ui.BrowseFragment
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(android.R.id.content, PlaybackFragment())
+                .replace(android.R.id.content, BrowseFragment())
                 .commit()
         }
     }
